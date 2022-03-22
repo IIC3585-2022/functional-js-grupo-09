af3dc42b67bd6c51dafe54ccc06a46922bae5fa5
mjespinoza <mjespinoza2@uc.cl>
2022-03-22 17:36:52 -0300

    error en dialog para pedir nombre jugador

--- src/calculators.js
+++ src/calculators.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-
+import * as readline from 'readline';
 
 import {
   pointsParser
@@ -20,34 +20,50 @@ export const enterPlay = (user, userPoints, userPlays) => {
 
 
 //funcion para editar el array de jugadores (no retorna)
-const addPlayers = (players, n_players, readline) => {
-  for( let i = 0; i < n_players; i++){
-    
-  }
-
-  
+const addPlayers = (players, name) => {
+  players.push({name: name, score: 501}); 
   
 }
 
-//funciones para manejar el dialogo:
+const enterPlayers = (players, n_players, input) => {
+  n_players = input;
+  for(let i = 1; i <= n_players; i++){
+    dialog(1, players, n_players);
+  }
 
-//1.funcion para pedir jugadas 
-
-
-//2.funcion para notificar resultado de las jugadas
-
-
-//3.funcion para pedir el numero de jugadores y sus nombres
-export const enterPlayers = (players, n_players, readline) => {
-  readline.question('Ingresar número de jugadores\n', number => {
-    if(number < 2){
-      console.log("Deben ser al menos dos jugadores, ¡Hasta la próxima!");
-    } else{
-      n_players = number;
-      addPlayers(players, n_players);
-    }
-    readline.close()
+}
+//funcion para manejar el dialogo
+export const dialog = (id, players, n_players) =>{
+  //codigo para dialogo por consola 
+  // ID = 0 -> pedir numero de jugadores 
+  // ID = 1 -> pedir nombre jugadores
+  const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
   })
+  
+  if (id ==0){
+    rl.question('Ingresar número de jugadores\n', number => {
+      if(number < 2){
+        console.log("Deben ser al menos dos jugadores, ¡Hasta la próxima!");
+      } else{
+        enterPlayers (players, n_players, number);   
+      }
+      rl.close();
+      
+    })
+  } else if (id == 1){
+      rl.question('Ingresar nombre jugador:\n', answer =>{
+        addPlayers(players, answer);
+        console.log('¡Hola ' + answer + '! tu puntaje inicial es 501\n');  
+        rl.close();
+      })
+      
+      
+    }
+    
+
+  
 
 }
 
--- src/main.js
+++ src/main.js
@@ -1,25 +1,22 @@
 //main juego 
-import * as readline from 'readline';
+
 import {
-  enterPlayers
+  dialog
   } from './calculators.js';
 
 function main (){
     console.log("¡Bienvenidxs!");
-
-    //codigo para dialogo por consola 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
-
     let players = []; // [{name: , score: 501},{},...]
     let n_players = 0; //numero de jugadores
 
     //preguntar por el numero de jugadores e iterar para preguntar los nombres 
-    enterPlayers(players, n_players, rl);
+    dialog(0, players, n_players);
     
     //comenzar juego 
+
+
+
+    
 
 
 }
