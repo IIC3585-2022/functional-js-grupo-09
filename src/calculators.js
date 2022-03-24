ce95e984dadfb7680ac24611d896b1cdc2a1c308
julio <jnandrade@uc.cl>
2022-03-24 17:17:43 -0300

    feat: win condition

--- src/calculators.js
+++ src/calculators.js
@@ -1,25 +1,30 @@
-import _ from 'lodash';
-import {
-  pointsParser
-} from './parsers.js';
+import _ from "lodash";
+import { pointsParser } from "./parsers.js";
 
-const calculatePoints = (userPoints, parsedPoints) => Math.abs(userPoints - parsedPoints);
+const calculatePoints = (userPoints, parsedPoints) =>
+  Math.abs(userPoints - parsedPoints);
 
 export const enterPlay = (user, userPoints, userPlays) => {
   let parsedPoints = _.chain(userPlays)
-    .map(play => pointsParser(play))
-    .reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-      0
-    )
-    .value()
+    .map((play) => pointsParser(play))
+    .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+    .value();
   return calculatePoints(userPoints, parsedPoints);
 };
 
 //funcion para editar el array de jugadores (no retorna)
-export const addPlayers = (players_array, name) => players_array.push([name, 501]);
+export const addPlayers = (players_array, name) =>
+  players_array.push([name, 501]);
 
+export const iterator = function* (index, n_players) {
+  yield (index + 1) % n_players;
+};
 
-export const iterator = function*(index, n_players){
-  yield (index +1)%(n_players)
-}
+export const checkWin = (array) => {
+  array.forEach((playerData) => {
+    let score = playerData[1];
+    let winner = playerData[0];
+    if (score == 0) return winner;
+  });
+  return false;
+};
--- src/initGame.js
+++ src/initGame.js
@@ -1,25 +1,20 @@
-import readline from 'readline';
-import {
-  addPlayers,
-  iterator
-} from './calculators.js';
+import readline from "readline";
+import { addPlayers, iterator, checkWin } from "./calculators.js";
 
-
-
-const Game = (players_array) =>{
-  //usar iterators 
+const Game = (players_array) => {
+  //usar iterators
   //let i= iterator(0, players_array.len)
-  //while(condicion de puntaje de jugadores){
-    //i.next().value 
+  let winner = checkWin(players_array);
+  while (!winner) {
+    //i.next().value
     //players.array[i]
     //ver tema de los dialogos
-  //};
-  
-
-}
+  }
+  console.log(`El juego ha terminado el ganador es: ${winner}`);
+};
 
 const enterPlayers = async (players_array, n_players) => {
-  if (n_players === 0){
+  if (n_players === 0) {
     // Here is stored the last players_array state
     //console.log(players_array);
     console.log("¡Que comienze el juego!");
@@ -29,36 +24,36 @@ const enterPlayers = async (players_array, n_players) => {
   await beginDialog(1, players_array, n_players);
 };
 
-const checkTwoOrMorePlayers = players_number => players_number >= 2 ? true : false;
+const checkTwoOrMorePlayers = (players_number) =>
+  players_number >= 2 ? true : false;
 
 //function to begin the dialog
 export const beginDialog = async (id, players_array, n_players) => {
-  //codigo para dialogo por consola 
-  // ID = 0 -> pedir numero de jugadores 
+  //codigo para dialogo por consola
+  // ID = 0 -> pedir numero de jugadores
   // ID = 1 -> pedir nombre jugadores
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
 
   if (id === 0) {
-    rl.question('Ingresa el número de jugadores:\n', number => {
-      if (!checkTwoOrMorePlayers(number)){
+    rl.question("Ingresa el número de jugadores:\n", (number) => {
+      if (!checkTwoOrMorePlayers(number)) {
         console.log("Deben ser al menos dos jugadores, ¡Hasta la próxima!");
         return rl.close();
-      };
+      }
       rl.close();
       enterPlayers(players_array, parseInt(number));
     });
-  };
+  }
 
-  if (id === 1){
-    rl.question('Ingresar nombre jugador:\n', player_name => {
+  if (id === 1) {
+    rl.question("Ingresar nombre jugador:\n", (player_name) => {
       addPlayers(players_array, player_name);
-      console.log('¡Hola ' + player_name + '! tu puntaje inicial es 501\n');
+      console.log("¡Hola " + player_name + "! tu puntaje inicial es 501\n");
       rl.close();
       enterPlayers(players_array, n_players - 1);
     });
-  };
+  }
 };
-
